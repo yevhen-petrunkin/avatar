6aa97a7bbd02e3b0a3f86010998343b2f9e5fa02
yevhen-petrunkin <110964764+yevhen-petrunkin@users.noreply.github.com>
2022-11-27 22:24:38 +0000

    Deploying to gh-pages from @ yevhen-petrunkin/avatar@c73dc135e12a05fc1d25ded9d1d5b95f42072b94 🚀

---
+++ index.3b6889a0.js
@@ -0,0 +1,2 @@
+fetch("https://attackontitanclient.netlify.app/#/characters/1").then((function(t){return console.log(t.json())})).then(console.log);
+//# sourceMappingURL=index.3b6889a0.js.map
---
+++ index.b9b4bc6d.js
@@ -0,0 +1,2 @@
+fetch("https://attackontitanclient.netlify.app/#/characters/1").then((t=>console.log(t.json()))).then(console.log);
+//# sourceMappingURL=index.b9b4bc6d.js.map
